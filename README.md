4a571348319678a10bc6afc3db8730710833cb8f
David Trussler <mail@davidtrussler.net>
2020-04-09 08:56:44 +0100

    Adds and reads state data in a component

--- README.md
+++ README.md
@@ -14,3 +14,4 @@ Based on Chapter 11 of Pro-React-16
 ### 2. [Creating a Stateful Component](https://github.com/davidtrussler/Pro-React-16_11_Stateful-components/tree/Create-Stateful-Component)
 
 - Converts SimpleButton from stateless to stateful component
+- Adds State Data via the contructor and reads it in the component
--- components/src/SimpleButton.js
+++ components/src/SimpleButton.js
@@ -1,13 +1,26 @@
 import React, {Component} from 'react'; 
 
 export class SimpleButton extends Component {
+	constructor(props) {
+		super(props); 
+
+		this.state = {
+			counter: 0, 
+			hasButtonBeenClicked: false
+		}
+	}
+
 	render() {
 		return (
 			<button 
 				className={this.props.className}
 				onClick={this.props.callback}
 				disabled={this.props.disabled === 'true' || this.props.disabled === true}
-			>{this.props.text}</button>
+			>
+				{this.props.text} 
+				{this.state.counter} 
+				{this.state.hasButtonBeenClicked && <div>Button Clicked!</div>}
+			</button>
 		)		
 	}
 }
